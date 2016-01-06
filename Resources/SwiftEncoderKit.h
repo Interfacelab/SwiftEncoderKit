497b221e4b32a790f27f332d7d73a2dbd84af8da
Jon Gilkison <jon.gilkison@gmail.com>
2016-01-06 14:07:54 +0700

    Initial commit

---
+++ Resources/SwiftEncoderKit.h
@@ -0,0 +1,19 @@
+//
+//  SwiftEncoderKit.h
+//  SwiftEncoderKit
+//
+//  Created by Jon Gilkison on 1/6/16.
+//  Copyright © 2016 Interfacelab LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftEncoderKit.
+FOUNDATION_EXPORT double SwiftEncoderKitVersionNumber;
+
+//! Project version string for SwiftEncoderKit.
+FOUNDATION_EXPORT const unsigned char SwiftEncoderKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftEncoderKit/PublicHeader.h>
+
+
